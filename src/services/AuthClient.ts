94c2c5a6360e2880c202409438d8a49e7cc1dd73
timothee Demares <timothee.demares@gmail.com>
2026-04-12 17:52:18 +0200

    fix: erasableSyntaxeOnly error

--- src/services/AuthClient.ts
+++ src/services/AuthClient.ts
@@ -32,11 +32,11 @@ const getBaseUrl = (): string => {
 };
 
 export class AuthError extends Error {
-  constructor(
-    public status: number,
-    message: string
-  ) {
+  status: number;
+
+  constructor(status: number, message: string) {
     super(message);
+    this.status = status;
   }
 }
 
